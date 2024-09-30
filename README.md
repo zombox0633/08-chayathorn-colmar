8963f83df55222dd22d13d3f1ffd4212086d8910
Zombox <ghostzb@outlook.com>
2024-09-30 10:04:38 +0700

    Move the file location and set up add nav

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 https://docs.google.com/spreadsheets/d/114DX9BzwwgAzB1wLvA7ycPIvUbOauHNRw_1XappmTHo/edit?gid=0#gid=0
+https://sshapiro1.github.io/Colmar_Academy/
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{html,js}", "./public/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mukta_light: ["Mukta", "sans-serif"],
+      },
+      colors: {
+        anti_flash_white: "#F3F5F5",
+        timber_wolf: "#D8D8D8",
+        battleship_gray: "#9B9B9B",
+        davy_gray: "#4A4A4A",
+      },
+    },
   },
   plugins: [],
-}
-
+};
